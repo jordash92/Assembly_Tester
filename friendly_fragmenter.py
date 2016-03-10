5c8647620d84454b64ab1b2100579b0f8e40aa45
Jordan Ashworth <jordanashworth492@gmail.com>
2016-03-10 18:39:17 +0000

    Better comments and option to quit script
    Updated to include more comments.
    Updated to have option to quit script if incorrect coverage is entered by user.

--- friendly_fragmenter.py
+++ friendly_fragmenter.py
@@ -3,13 +3,16 @@
 from Bio import Entrez
 from Bio import SeqIO
 import random
+import sys
+
 Entrez.email = "j.ashworth2@ncl.ac.uk"
+
+
 
 def get_record(id_number):
     handle = Entrez.efetch(db="nucleotide", rettype="fasta", retmode="text", id="16271976")
     seq_record = SeqIO.read(handle, "fasta")
     handle.close()
-    #print ("%s with %i features and length of %s bp" % (seq_record.id, len(seq_record.features), len(seq_record.seq)))
     return seq_record.seq
 
 def random_fragment(genome, length):
@@ -23,70 +26,59 @@ def fragment(file, samples, id):
         my_file.write(">fragment{}\n".format(i+1))
         my_file.write(str(random_fragment(my_genome, 36)) + "\n")
     my_file.close()
+    return my_genome
 
-# Fragment genome: put fragments in a file called fragments.fasta, create random 5000000 fragments (will give x100 coverage) of genome associated with genbank id 16271976
+# Parameters for the assembly chosen depending on coverage stated by user
 # Coverage cutoff and expected coverage were calculated using R, while the k value was calcualted using Velvet Advisor
 
 def parameters(coverage):
-    if coverage == "40":
-        reads = 2000000
-        k_value = 19
-        frag_file = 'friendly_fragments_40.fasta'
-        cov_cutoff = 18
-        exp_cov = 19
-        print "Your desired coverage is 40. The genome will now be cut into 2 million fragments."
-    elif coverage == "60":
-        reads = 3000000
-        k_value = 25
-        frag_file = 'friendly_fragments_60.fasta'
-        cov_cutoff = 18
-        exp_cov = 19
-        print "Your desired coverage is 60. The genome will now be cut into 3 million fragments."
-    elif coverage == "80":
-        reads = 4000000
-        k_value = 29
-        frag_file = 'friendly_fragments_80.fasta'
-        cov_cutoff = 31
-        exp_cov = 35
-        print "Your desired coverage is 80. The genome will now be cut into 4 million fragments."
-    elif coverage == "100":
-        reads = 5000000
-        k_value = 29
-        frag_file = 'friendly_fragments_100.fasta'
-        cov_cutoff = 20
-        exp_cov = 21
-        print "Your desired coverage is 100. The genome will now be cut into 5 million fragments."
-    elif coverage == "120":
-        reads = 6000000
-        k_value = 31
-        frag_file = 'friendly_fragments_120.fasta'
-        cov_cutoff = 18
-        exp_cov = 19
-        print "Your desired coverage is 120. The genome will now be cut into 6 million fragments."
-    elif coverage == "140":
-        reads = 7000000
-        k_value = 31
-        frag_file = 'friendly_fragments_140.fasta'
-        cov_cutoff = 21
-        exp_cov = 22
-        print "Your desired coverage is 140. The genome will now be cut into 7 million fragments."
-    else:
-        print "ERROR! You have entered an unsuitable coverage, please try again:"
-        my_coverage = raw_input('Please enter desired genome coverage, this can be 40, 60, 80, 100, 120 or 140 times. For example, if you would like 100x coverage to be generated type 100 and press enter: ')
-        parameters(my_coverage)
+    reference = {"40": (2000000, 19, 18, 19),
+                 "60": (3000000, 29, 20, 21),
+                 "80": (4000000, 29, 31, 35),
+                 "100": (5000000, 29, 20, 21),
+                 "120": (6000000, 31, 18, 19),
+                 "140": (7000000, 31, 21, 22)}
+    # if incorrect coverage stated by user, asked to enter new coverage or exit program
+    while coverage not in reference:
+        print "ERROR! You have entered an unsuitable coverage, please try again"
+        user_retry = raw_input("Would you like to enter a new coverage [1] or leave Assembly Tester [2]?")
+        if user_retry == "1":
+            # if user chooses to enter new coverage, replaces old coverage for new coverage
+            coverage = raw_input('Please enter desired genome coverage, this can be 40, 60, 80, 100, 120 or 140 times. For example, if you would like 100x coverage to be generated type 100 and press enter: ')
+        # terminate script if user chooses to exit program
+        elif user_retry == "2":
+            sys.exit()
+        # user did not enter 1 or 2 so asks to try again
+        else:
+            print "Try again"
+    # for coverage selected by user, associate appropriate values in dictonary to following vairables:
+    reads, k_value, cov_cutoff, exp_cov = reference[coverage]
+    # name file containing fragments is association to coverage set by user
+    frag_file = 'friendly_fragments_{}.fasta'.format(coverage)
+    print dots, "\nYour desired coverage is {}x.\nThe genome of Haemophilus influenzae will now be cut into {} million fragments.".format(coverage, str(reads)[0])
     return frag_file, reads, k_value, cov_cutoff, exp_cov
 
-#if __name__ == '__main__':
+dots = "=========================================================="
+
+print dots, "\n * * * * * * * Welcome to Assembly Tester! * * * * * * *\n", dots
+
+# Ask user for desired coverage. Waits for raw input from user before continuing.
 
 my_coverage = raw_input('Please enter desired genome coverage, this can be 40, 60, 80, 100, 120 or 140 times. For example, if you would like 100x coverage to be generated type 100 and press enter: ')
 
+# Call parameters to select those appropriate from dictionary for desired coverage (calculated using Velvet Advisor)
+
 frag_file, reads, k_value, cov_cutoff, exp_cov = parameters(my_coverage)
+
+# Call fragment genome
 
 fragment(frag_file, reads, "16271976")
 
-print "============================================="
-print "reads: ", reads
-print "k-value for assembly: ", k_value
-print "coverage cutoff: ", cov_cutoff
-print "expected coverage: ", exp_cov
-print "Genome has been fragmented! Fragments have been saved to: ", frag_file
+print dots, "\nParameters for the assembly:"
+print "reads:", reads
+print "k-value for assembly:", k_value
+print "coverage cutoff:", cov_cutoff
+print "expected coverage:", exp_cov
+print "Haemophilus influenzae has been sucessfully fragmented!\nFragments have been saved to:", frag_file
+
+
